8f1708503205a73c2efcc03b1d9c5dcacf612ac4
gracia <graciakleijnen@hotmail.com>
2017-10-24 20:19:52 +0200

    Updated the viewNearbyRequests and viewNearbyBuddies methods.

--- week1-homework.js
+++ week1-homework.js
@@ -2,13 +2,13 @@ let allOpenRequests = [];
 
 
 class CarryBuddy{
-	constructor(name, location, email, requests, ratings, events){
+	constructor(name, location, email, requests, ratings, upcomingEvents){
 		this.name = name;
 		this.location = location;
 		this.email = email;
 		this.requests = []; 
 		this.ratings = [];
-		this.events = [];
+		this.upcomingEvents = [];
 		this.average_rating = 0;
 		this.sum = 0;
 		this.num = 0;
@@ -19,33 +19,33 @@ class CarryBuddy{
 		allOpenRequests.push(request);
 	}
 
-	viewNearbyRequests(...location){  // works with ...? to search +1 locations
-		if (allOpenRequests.length < 0) {
+	viewNearbyRequests(location){  // look for one location at a time
+		if (allOpenRequests.length < 0) { // incorrect, should be if length AT LOCATION < 0
 			console.log("No carry buddies are needed here right now.")
 		}
 		else {
 			console.log("Here are the open requests in " + arguments[0] + ":");
-			let requestsBeforeFiltering = [];
-			requestsBeforeFiltering = allOpenRequests;
-
-			filteredRequests = location => requestsBeforeFiltering.find(function(o){
-				return o.location === location;
+			let openRequestsByLocation = allOpenRequests.filter(function(loc) {
+				return loc.location === location
 			});
-			console.log(filteredRequests);
-
-		}
+			console.log(openRequestsByLocation);
+		} // extra: return only item, location, name, date, time.
 
 	}
 
-	viewNearbyBuddies(location){  // do the same above, but for ppl
-		// return list of ALL people
-		// in location X
-		// return only name, location, email, average rating
+	viewNearbyBuddies(location){		
+		let nearbyBuddies = carrybuddies.filter(function(el) {
+			return el.location === location
+		});
+		console.log(nearbyBuddies);
+		// extra: return only name, location, email, average rating per buddy
+		// extra: build if-else for if AT LOCATION no buddies exist
+		//with else "Sorry, no one around here."
 	}
 
 	offerHelp(buddy, request) {
 		// person2 chooses to help person1 in UI push a button..
-		// set buddy1.request.whatevername.buddyFound to True
+		// set buddy1.request.whatevername.buddyFound to True: where to build this in?
 		// if above set to True: buddy1.requests.whatevername remove from reuqests
 		// delete from AllOpenRequests[];
 		// to both buddies: receive confirmation with all info.
@@ -58,11 +58,11 @@ class CarryBuddy{
 		console.log("Buddy X and X will meet at date, location to carry item X.");
 	}
 
-	writeFeedback(buddy, rating, comment){
-		buddy.receiveFeedback(new Feedback(rating, comment, this))
+	writeReview(buddy, rating, comment){ // by monikks
+		buddy.receiveReview(new Review(rating, comment, this))
 	}
 
-	receiveFeedback(rating){ // by monikks
+	receiveReview(rating){ // by monikks
 		this.ratings.push(rating);
 		this.sum = this.sum + rating.rating;
 		this.num += 1;
@@ -74,9 +74,10 @@ class CarryBuddy{
 }
 
 class Request {
-	constructor(itemName, location, date, time, buddyFound){
+	constructor(itemName, owner, location, date, time, buddyFound){
 		this.itemName = itemName;
-		this.location = location;  // do I need this here?
+		this.owner = owner; // SET TO OWNER whos array it is pushed to
+		this.location = location;  // do I need this?
 		this.date = date;
 		this.time = time;
 		this.buddyFound = false;
@@ -97,7 +98,7 @@ getCarryBuddyByName = name => carrybuddies.find(function(o){return o.name === na
 
 carrybuddy1 = new CarryBuddy("Gracia", "Berlin", "g@racia.com");
 carrybuddy2 = new CarryBuddy("Piet", "Berlin", "p@iet.com");
-carrybuddy3 = new CarryBuddy("Henk","Stuttgart","h@enk.com");
+carrybuddy3 = new CarryBuddy("Henk", "Stuttgart","h@enk.com");
 
 carrybuddies.push(carrybuddy1);
 carrybuddies.push(carrybuddy2);
