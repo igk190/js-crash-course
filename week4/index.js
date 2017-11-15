b6b4ac06cce2dc2c4d6bb6c8e75e483b8ef2ffb0
igk190 <graciakleijnen@hotmail.com>
2017-11-16 00:18:10 +0100

    General updates. Axios.delete and create person not yet working.

--- week4/index.js
+++ week4/index.js
@@ -1,7 +1,39 @@
 const fs = require('fs');
 const Seeder = require('./seeder')
+const express = require('express')
 const Database = require('./Data/database')
+const bodyParser = require('body-parser')
+const CarryBuddyService = require('./services/carrybuddy-service')
+
 const app = express();
+
+app.set('view engine', 'pug') // when render, use pug
+
+app.use(bodyParser.json())
+
+app.get('/',  (req, res, next) => {
+    res.render('index')
+    
+    // res.sendFile(__dirname + '/index.html')
+    
+    // res.send(await CarryBuddyService.findAll())
+}) 
+
+app.get('/carrybuddy/all', async (req, res, next) => {
+    const carrybuddies = await CarryBuddyService.findAll()
+
+    res.render('carrybuddy', { carrybuddies })
+})
+
+app.post('/carrybuddy', async (req, res, next) => {
+    const carrybuddy = await CarryBuddyService.add(req.body)
+    res.send(carrybuddy)
+})
+
+app.delete('/carrybuddy/:id', async (req, res, next) => {
+    await CarryBuddyService.del(req.params.id) // check ID!!!!!!!
+    res.send('ok!')
+})
 
 function main() {
     
@@ -13,4 +45,8 @@ function main() {
     
 }
 
-main()
+main() // not in wk 4 index
+
+app.listen(5000, () => { // cb: method that gets executed after an event
+    console.log('Server listening')
+})
